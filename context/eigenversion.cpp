372e2a154aabc6725883c249095a1e9a698a0599
Rania Ahmed <ranianr91@gmail.com>
2022-10-22 13:14:23 +0200

    Add Eigen Dockerfile

---
+++ context/eigenversion.cpp
@@ -0,0 +1,12 @@
+// Source: https://dritchie.github.io/csci2240/assignments/eigen_tutorial.pdf
+# include <Eigen/Core>
+# include <iostream>
+using namespace std ;
+using namespace Eigen ;
+
+int main () {
+  cout << " Eigen version : " << EIGEN_MAJOR_VERSION << "."
+  << EIGEN_MINOR_VERSION << endl ;
+  return 0;
+}
+
